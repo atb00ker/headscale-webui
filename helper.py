eed7be8a08c2b71acab25072c4d300083b82934e
Ajay Tripathi <ajay39in@gmail.com>
2023-05-06 01:53:38 +0530

    [deploy] Allow changing data directory in bare-metal setups

--- helper.py
+++ helper.py
@@ -4,6 +4,7 @@ import os, headscale, requests, logging
 from flask import Flask
 
 LOG_LEVEL = os.environ["LOG_LEVEL"].replace('"', '').upper()
+DATA_DIRECTORY = os.environ["DATA_DIRECTORY"].replace('"', '') if os.environ["DATA_DIRECTORY"] else "/data"
 # Initiate the Flask application and logging:
 app = Flask(__name__, static_url_path="/static")
 match LOG_LEVEL:
@@ -158,12 +159,12 @@ def access_checks():
     # Return an error message if things fail.
     # Return a formatted error message for EACH fail.
     checks_passed   = True # Default to true.  Set to false when any checks fail.
-    data_readable   = False # Checks R permissions of /data
-    data_writable   = False # Checks W permissions of /data
+    data_readable   = False # Checks R permissions of DATA_DIRECTORY
+    data_writable   = False # Checks W permissions of DATA_DIRECTORY
     data_executable = False # Execute on directories allows file access
-    file_readable   = False # Checks R permissions of /data/key.txt
-    file_writable   = False # Checks W permissions of /data/key.txt
-    file_exists     = False # Checks if /data/key.txt exists
+    file_readable   = False # Checks R permissions of DATA_DIRECTORY/key.txt
+    file_writable   = False # Checks W permissions of DATA_DIRECTORY/key.txt
+    file_exists     = False # Checks if DATA_DIRECTORY/key.txt exists
     config_readable = False # Checks if the headscale configuration file is readable
 
 
@@ -176,32 +177,32 @@ def access_checks():
         checks_passed = False
         app.logger.critical("Headscale URL: Response 200: FAILED")
 
-    # Check: /data is rwx for 1000:1000:
-    if os.access('/data/', os.R_OK):  data_readable = True
+    # Check: DATA_DIRECTORY is rwx for 1000:1000:
+    if os.access(DATA_DIRECTORY, os.R_OK):  data_readable = True
     else:
-        app.logger.critical("/data READ: FAILED")
+        app.logger.critical(f"{DATA_DIRECTORY} READ: FAILED")
         checks_passed = False
-    if os.access('/data/', os.W_OK):  data_writable = True
+    if os.access(DATA_DIRECTORY, os.W_OK):  data_writable = True
     else:
-        app.logger.critical("/data WRITE: FAILED")
+        app.logger.critical(f"{DATA_DIRECTORY} WRITE: FAILED")
         checks_passed = False
-    if os.access('/data/', os.X_OK):   data_executable = True
+    if os.access(DATA_DIRECTORY, os.X_OK):   data_executable = True
     else:
-        app.logger.critical("/data EXEC: FAILED")
+        app.logger.critical(f"{DATA_DIRECTORY} EXEC: FAILED")
         checks_passed = False
 
-    # Check: /data/key.txt exists and is rw:
-    if os.access('/data/key.txt', os.F_OK):
+    # Check: DATA_DIRECTORY/key.txt exists and is rw:
+    if os.access(os.path.join(DATA_DIRECTORY, "key.txt"), os.F_OK):
         file_exists = True
-        if os.access('/data/key.txt', os.R_OK): file_readable = True
+        if os.access(os.path.join(DATA_DIRECTORY, "key.txt"), os.R_OK): file_readable = True
         else:
-            app.logger.critical("/data/key.txt READ: FAILED")
+            app.logger.critical(f"{os.path.join(DATA_DIRECTORY, 'key.txt')} READ: FAILED")
             checks_passed = False
-        if os.access('/data/key.txt', os.W_OK):  file_writable = True
+        if os.access(os.path.join(DATA_DIRECTORY, "key.txt"), os.W_OK):  file_writable = True
         else:
-            app.logger.critical("/data/key.txt WRITE: FAILED")
+            app.logger.critical(f"{os.path.join(DATA_DIRECTORY, 'key.txt')} WRITE: FAILED")
             checks_passed = False
-    else: app.logger.error("/data/key.txt EXIST: FAILED - NO ERROR")
+    else: app.logger.error(f"{os.path.join(DATA_DIRECTORY, 'key.txt')} EXIST: FAILED - NO ERROR")
 
     # Check: /etc/headscale/config.yaml is readable:
     if os.access('/etc/headscale/config.yaml', os.R_OK):  config_readable = True
@@ -237,58 +238,58 @@ def access_checks():
         message_html += format_message("Error", "/etc/headscale/config.yaml not readable", message)
 
     if not data_writable:
-        app.logger.critical("/data folder is not writable")
-        message = """
-        <p>/data is not writable.  Please ensure your
-        permissions are correct. /data mount should be writable
+        app.logger.critical(f"{DATA_DIRECTORY} folder is not writable")
+        message = f"""
+        <p>{DATA_DIRECTORY} is not writable.  Please ensure your
+        permissions are correct. {DATA_DIRECTORY} mount should be writable
         by UID/GID 1000:1000.</p>
         """
 
-        message_html += format_message("Error", "/data not writable", message)
+        message_html += format_message("Error", f"{DATA_DIRECTORY} not writable", message)
 
     if not data_readable:
-        app.logger.critical("/data folder is not readable")
-        message = """
-        <p>/data is not readable.  Please ensure your
-        permissions are correct. /data mount should be readable
+        app.logger.critical(f"{DATA_DIRECTORY} folder is not readable")
+        message = f"""
+        <p>{DATA_DIRECTORY} is not readable.  Please ensure your
+        permissions are correct. {DATA_DIRECTORY} mount should be readable
         by UID/GID 1000:1000.</p>
         """
 
-        message_html += format_message("Error", "/data not readable", message)
+        message_html += format_message("Error", f"{DATA_DIRECTORY} not readable", message)
 
     if not data_executable:
-        app.logger.critical("/data folder is not readable")
+        app.logger.critical(f"{DATA_DIRECTORY} folder is not readable")
         message = """
-        <p>/data is not executable.  Please ensure your
-        permissions are correct. /data mount should be readable
+        <p>{DATA_DIRECTORY} is not executable.  Please ensure your
+        permissions are correct. {DATA_DIRECTORY} mount should be readable
         by UID/GID 1000:1000. (chown 1000:1000 /path/to/data && chmod -R 755 /path/to/data)</p>
         """
 
-        message_html += format_message("Error", "/data not executable", message)
+        message_html += format_message("Error", f"{DATA_DIRECTORY} not executable", message)
 
 
     if file_exists:
         # If it doesn't exist, we assume the user hasn't created it yet.
         # Just redirect to the settings page to enter an API Key
         if not file_writable:
-            app.logger.critical("/data/key.txt is not writable")
-            message = """
-            <p>/data/key.txt is not writable.  Please ensure your
-            permissions are correct. /data mount should be writable
+            app.logger.critical(f"{os.path.join(DATA_DIRECTORY, 'key.txt')} is not writable")
+            message = f"""
+            <p>{os.path.join(DATA_DIRECTORY, 'key.txt')} is not writable.  Please ensure your
+            permissions are correct. {DATA_DIRECTORY} mount should be writable
             by UID/GID 1000:1000.</p>
             """
 
-            message_html += format_message("Error", "/data/key.txt not writable", message)
+            message_html += format_message("Error", f"{os.path.join(DATA_DIRECTORY, 'key.txt')} not writable", message)
 
         if not file_readable:
-            app.logger.critical("/data/key.txt is not readable")
-            message = """
-            <p>/data/key.txt is not readable.  Please ensure your
-            permissions are correct. /data mount should be readable
+            app.logger.critical(f"{os.path.join(DATA_DIRECTORY, 'key.txt')} is not readable")
+            message = f"""
+            <p>{os.path.join(DATA_DIRECTORY, 'key.txt')} is not readable.  Please ensure your
+            permissions are correct. {DATA_DIRECTORY} mount should be readable
             by UID/GID 1000:1000.</p>
             """
 
-            message_html += format_message("Error", "/data/key.txt not readable", message)
+            message_html += format_message("Error", f"{os.path.join(DATA_DIRECTORY, 'key.txt')} not readable", message)
 
     return message_html
 
